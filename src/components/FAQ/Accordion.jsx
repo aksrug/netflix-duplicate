50302a98239726b2c2336dd77ed3e7cb16d32074
FIRSTNAME LASTNAME <YOUR@EMAIL.COM>
2024-08-23 13:22:04 +0300

    upd

--- src/components/FAQ/Accordion.jsx
+++ src/components/FAQ/Accordion.jsx
@@ -35,7 +35,7 @@ const Accordion = () => {
       <h1 className="faq-heading">Frequently Asked Questions</h1>
       <AccordionItem
         title="What is Netflix?"
-        content="Netflix is a streaming service that offers a wide variety of award-winning TV shows, movies, anime, documentaries, and more on thousands of internet-connected devices.
+        content="Netflix is a streaming service that offers a wide variety of award-winning TV shows,<br/> movies, anime, documentaries, and more on thousands of internet-connected <br /> devices.
 
 You can watch as much as you want, whenever you want without a single commercial – all for one low monthly price. There's always something new to discover and new TV shows and movies are added every week!"
       />
